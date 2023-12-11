c430aa87c2770a719dbacf74299dd8a8862e436c
chandraboni <141957826+chandraboni@users.noreply.github.com>
2023-12-11 15:41:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "chandra_vpc" {
   count = 3
   cidr_block = "10.0.0.0/16"
   tags = {
-   Name = "VPC_${count.index}" 
+   Name = "VPC_${count.index+1}" 
 //Name = "chandra_vpc${count.index}"
   }
 }
